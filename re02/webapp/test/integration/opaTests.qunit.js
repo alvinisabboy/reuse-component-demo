182db142c6eacf1bc32505667b4bbf651cca0e91
Mai <alva.mai@sap.com>
2024-08-01 11:21:14 +0800

    commit local project

--- re02/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["demo/suresh/reusecomp/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- re02/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"demosuresh/reusecomp/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
